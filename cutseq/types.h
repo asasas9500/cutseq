497fb5539b04efd6ff7e637a6fd06a126bd9688d
asasas9500 <asasas9500@gmail.com>
2022-03-26 12:19:45 -0300

    Add remaining types

--- cutseq/types.h
+++ cutseq/types.h
@@ -22,3 +22,44 @@ struct NEW_CUTSCENE
 	long camera_offset;
 	ACTORME actor_data[10];
 };
+
+struct RTDECODE
+{
+	ulong length;
+	ulong off;
+	ushort counter;
+	ushort data;
+	uchar decodetype;
+	uchar packmethod;
+	ushort padfuck;
+};
+
+struct PACKNODE
+{
+	short xrot_run;
+	short yrot_run;
+	short zrot_run;
+	short xkey;
+	short ykey;
+	short zkey;
+	RTDECODE decode_x;
+	RTDECODE decode_y;
+	RTDECODE decode_z;
+	ulong xlength;
+	ulong ylength;
+	ulong zlength;
+	char* xpacked;
+	char* ypacked;
+	char* zpacked;
+};
+
+struct NODELOADHEADER
+{
+	short xkey;
+	short ykey;
+	short zkey;
+	short packmethod;
+	short xlength;
+	short ylength;
+	short zlength;
+};
