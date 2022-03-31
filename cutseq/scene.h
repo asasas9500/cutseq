ece82f5c6d603b81ae614d9bab0a67381461983a
asasas9500 <asasas9500@gmail.com>
2022-03-31 00:49:34 -0300

    Supply a prototype

--- cutseq/scene.h
+++ cutseq/scene.h
@@ -20,4 +20,5 @@ int TraverseActorHierarchy(FbxAnimLayer* layer, FbxNode* node, FRAME_DATA* playe
 int PackActor(FbxAnimLayer* layer, FbxNode* node, FRAME_DATA* player);
 void TransformChannel(float m, FbxArray<float>* channel);
 int PackCamera(FbxAnimLayer* layer, FbxNode* node, FRAME_DATA* player);
+int Pack(FbxAnimLayer* layer, FbxCamera* cam, FbxMesh** actor, FRAME_DATA* player);
 void Convert(SETUP_STRUCT* cfg);
