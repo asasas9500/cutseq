19418a8d126b208fc00ceb8f30962dfddda9a256
asasas9500 <asasas9500@gmail.com>
2022-04-05 12:05:28 -0300

    Fix rotation source and scale

--- cutseq/scene.cpp
+++ cutseq/scene.cpp
@@ -206,10 +206,10 @@ int TraverseActorHierarchy(FbxAnimLayer* layer, FbxNode* node, FRAME_DATA* playe
 	if (!ProcessProperty(layer, &node->LclRotation, FBXSDK_CURVENODE_COMPONENT_X, 2.8444444444F, &player->seq, &header->xkey, &header->xlength))
 		return 0;
 
-	if (!ProcessProperty(layer, &node->LclRotation, FBXSDK_CURVENODE_COMPONENT_Y, -2.8444444444F, &player->seq, &header->ykey, &header->ylength))
+	if (!ProcessProperty(layer, &node->LclRotation, FBXSDK_CURVENODE_COMPONENT_Z, -2.8444444444F, &player->seq, &header->ykey, &header->ylength))
 		return 0;
 
-	if (!ProcessProperty(layer, &node->LclRotation, FBXSDK_CURVENODE_COMPONENT_Z, -2.8444444444F, &player->seq, &header->zkey, &header->zlength))
+	if (!ProcessProperty(layer, &node->LclRotation, FBXSDK_CURVENODE_COMPONENT_Y, 2.8444444444F, &player->seq, &header->zkey, &header->zlength))
 		return 0;
 
 	for (int i = 0; i < node->GetChildCount(); i++)
