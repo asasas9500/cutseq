8ab8e6ccb217a8406a895c3e72113b906de422ab
asasas9500 <asasas9500@gmail.com>
2022-04-07 15:35:55 -0300

    Delete useless types

--- cutseq/types.h
+++ cutseq/types.h
@@ -30,36 +30,6 @@ struct NEW_CUTSCENE
 	ACTORME actor_data[10];
 };
 
-struct RTDECODE
-{
-	ulong length;
-	ulong off;
-	ushort counter;
-	ushort data;
-	uchar decodetype;
-	uchar packmethod;
-	ushort padfuck;
-};
-
-struct PACKNODE
-{
-	short xrot_run;
-	short yrot_run;
-	short zrot_run;
-	short xkey;
-	short ykey;
-	short zkey;
-	RTDECODE decode_x;
-	RTDECODE decode_y;
-	RTDECODE decode_z;
-	ulong xlength;
-	ulong ylength;
-	ulong zlength;
-	char* xpacked;
-	char* ypacked;
-	char* zpacked;
-};
-
 struct NODELOADHEADER
 {
 	short xkey;
